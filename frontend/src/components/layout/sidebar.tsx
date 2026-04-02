01620b6bb47ea7142c984a49324c5cf2fa0b516e
Saad Mazhar <92235123+saadmazhar03@users.noreply.github.com>
2026-04-02 15:54:32 +0530

    refactor: remove unused useRef import from sidebar component

--- frontend/src/components/layout/sidebar.tsx
+++ frontend/src/components/layout/sidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { useRouter, useParams } from 'next/navigation'
 import { signOut, updateProfile } from 'firebase/auth'
